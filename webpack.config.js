efc3e161a99d30ed81eddf4337ca621f85312d66
Davy'd <pdaswift3009@gmail.com>
2017-12-02 12:49:42 +0100

    add extract for css from bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: './dist/app.bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }),
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Project Demo",
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true
+        }),
+        new ExtractTextPlugin({
+            filename: "app.css",
+            // disabled: false,
+            // allChunks: true
+        }),
+    ]
+}
